ec1067d58a46352ffeb39e28b9360c4464e64ade
Alexander Martin <hi@droqen.com>
2023-06-20 20:11:58 -0400

    test00-child test, final test of local_to_parent

--- test00-child/src/server.rs
+++ test00-child/src/server.rs
@@ -1,64 +1,73 @@
 use ambient_api::{
     components::core::{
-        app::main_scene,
+        app::{main_scene, name},
         camera::aspect_ratio_from_window,
         ecs::{children, parent},
-        physics::{
-            character_controller_height, character_controller_radius, physics_controlled,
-        },
         player::{player, user_id},
         primitives::{cube},
-        rendering::{color},
+        rendering::color,
         transform::{lookat_target, translation, local_to_parent},
     },
     concepts::{make_perspective_infinite_reverse_camera, make_transformable, make_sphere},
     prelude::*,
 };
 
+const RED3 : Vec3 = vec3(1., 0., 0.);
+const GRN3 : Vec3 = vec3(0., 1., 0.);
+const BLU3 : Vec3 = vec3(0., 0., 1.);
+const WHT3 : Vec3 = vec3(1., 1., 1.);
+
 #[main]
 pub fn main() {
-    let bigCamera = Entity::new()
+    Entity::new()
         .with_merge(make_perspective_infinite_reverse_camera())
         .with(aspect_ratio_from_window(), EntityId::resources())
         .with_default(main_scene())
         .with(translation(), vec3(5., 5., 4.))
         .with(lookat_target(), vec3(0., 0., 0.))
         .spawn();
+    
+    spawn_parent_child_pair(vec3(0., 0., 0.), RED3, "red", false, false);
+    spawn_parent_child_pair(vec3(0., -2., 0.), GRN3, "green", true, false);
+    spawn_parent_child_pair(vec3(0., -4., 0.), BLU3, "blue", false, true);
+    spawn_parent_child_pair(vec3(0., -6., 0.), WHT3, "white", true, true);
 
-    spawn_query((player(), user_id())).bind(move |players| {
-        for (id, (_, uid)) in players {
-            let playerAdditionalPiece = Entity::new()
-                // .with_merge(make_perspective_infinite_reverse_camera())
-                // .with(aspect_ratio_from_window(), EntityId::resources())
-                .with_merge(make_sphere())
-                .with_default(main_scene())
-                .with(user_id(), uid)
-                .with(translation(), Vec3::Z * 2.)
-                .with(parent(), id)
-                .with_default(local_to_parent())
-                // .with(rotation(), Quat::from_rotation_x(PI / 2.))
-                .spawn();
-
-            entity::add_components(
-                id,
-                Entity::new()
-                    .with_merge(make_transformable())
-                    .with_default(cube())
-                    .with(color(), Vec4::ONE)
-                    // .with(character_controller_height(), 2.)
-                    // .with(character_controller_radius(), 0.5)
-                    // .with_default(physics_controlled())
-                    .with(children(), vec![playerAdditionalPiece])
-            );
+    query((translation(), color())).requires(children()).each_frame(|parents|{
+        for (ent, (_pos, _col)) in parents {
+            entity::mutate_component(ent, translation(), move |mut pos|{
+                // println!("{0:?} moved to -> {1:?}", _col, pos);
+                if pos.x < -5. {
+                    *pos += vec3(10., 0., 0.);
+                } else {
+                    *pos += vec3(-0.1, 0., 0.);
+                }
+            });
+            entity::mutate_component(ent, rotation(), move |mut rot|{
+                *rot *= Quat::from_rotation_z(-0.1);
+            });
         }
     });
+}
 
-    query((player(), translation())).each_frame(|players|{
-        for (player, (_player_component, _pos)) in players {
-            physics::move_character(player, vec3(-1., 0., 0.), 100., frametime());
-            entity::set_component(player, translation(), _pos + vec3(-0.1, 0., 0.));
-        }
-    });
+fn spawn_parent_child_pair(startPos : Vec3, color3 : Vec3, parent_name : &str, with_cube : bool, with_local_to_world : bool) -> EntityId {
+    let a = make_transformable()
+        .with(translation(), startPos)
+        .with(color(), color3.extend(1.))
+        .with(name(), parent_name.to_string())
+        .spawn();
 
-    println!("Hello, Ambient!");
+    if with_cube { entity::add_component(a, cube(), ()); }
+    if with_local_to_world { entity::add_component(a, local_to_world(), Mat4::IDENTITY); }
+
+    let b = Entity::new()
+        .with_default(cube())
+        .with(translation(), Vec3::Z * 2.)
+        .with(color(), color3.extend(1.))
+        .with(parent(), a)
+        .with_default(local_to_parent())
+        .spawn();
+
+    entity::add_component(a, children(), vec![b]);
+
+    return a;
 }
